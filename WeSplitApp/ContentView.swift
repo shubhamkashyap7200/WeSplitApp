af11bdae753d133ef074fdaf7898e0e6c46c87c6
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-07-20 21:54:40 +0530

    Learned about Navigation Stack

--- WeSplitApp/ContentView.swift
+++ WeSplitApp/ContentView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    @State private var name = ""
+    let students: [String] = ["Harry", "Hermonie", "Ron" , "Draco", "Goyle"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
-        Form {
-            Text("Hello, world!")
+        //        Button("Tap Count :: \(tapCount)") {
+        //            tapCount += 1
+        //        }
+        
+        //        Form {
+        //            TextField("Enter name", text: $name)
+        //            Text("Your name is \(name)")
+        //        }
+        
+        //        Form {
+        //            ForEach(0..<100) { number in
+        //                Text("Row \(number)")
+        //            }
+        //        }
+        
+        NavigationStack {
+            Form {
+                Picker("Select Student name", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+            
+            .navigationTitle("Student Database")
         }
+        
     }
+    
 }
 
 #Preview {

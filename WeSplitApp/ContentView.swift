8765c3f6c0b75d69404855c1f28c391eba16e72b
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-07-20 23:00:44 +0530

    We Split completed with all challenges

--- WeSplitApp/ContentView.swift
+++ WeSplitApp/ContentView.swift
@@ -15,6 +15,16 @@ struct ContentView: View {
     @FocusState private var amountIsFocused: Bool
     let tipPercentages = [0, 10, 15, 20, 25]
     
+    let currencyCalculator = Locale.current.currency?.identifier ?? "USD"
+    
+    var grandTotalAmount:Double {
+        let tipSelection: Double = Double(tipPercentage)
+        let tipValue = (checkAmount / 100) * tipSelection
+        let grandTotalAmount = checkAmount + tipValue
+
+        return grandTotalAmount
+    }
+    
     var totalPerPerson : Double {
         // calculate the total amount
         let peopleCount: Double = Double(numberOfPeople)
@@ -29,8 +39,11 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             Form {
+                
+                // MARK: Amount Selection
+
                 Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    TextField("Amount", value: $checkAmount, format: .currency(code: currencyCalculator))
                         .keyboardType(.decimalPad)
                         .focused($amountIsFocused)
                     
@@ -41,27 +54,48 @@ struct ContentView: View {
                     }
                 }
                 
+                // MARK: Tip Percentage Selection
+
                 Section {
                     Picker("Select Tip Percentage", selection: $tipPercentage) {
                         ForEach(tipPercentages, id: \.self) {
                             Text($0, format: .percent)
                         }
+                        
+//                        ForEach(0..<101, id: \.self) {
+//                            Text($0, format: .percent)
+//                        }
                     }
                     .pickerStyle(.segmented)
                 } header: {
                     Text("How much tip you want to leave?")
                 }
                 
+                // MARK: Grand Total Amount = checkAmount + tipAmount
+                Section {
+                    Text(grandTotalAmount, format: .currency(code: currencyCalculator))
+                } header: {
+                    Text("Grand total amount")
+                }
+
+                
+                // MARK: Amount per person
+
                 Section {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                } header: {
+                    Text("Amount per person")
                 }
+                
             }
             
             .navigationTitle("Bill Splitter")
             .toolbar {
                 ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
                     Button("Done") {
-                        amountIsFocused = true
+                        amountIsFocused = false
                     }
                 }
             }
